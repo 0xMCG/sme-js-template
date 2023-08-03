ac67f7d8d7e25c70bc268ab116f4f4b25f12f368
moshui <sme_moshui@outlook.com>
2023-08-03 10:51:19 +0800

    add types

---
+++ src/abi/Seaport_VRF.ts
@@ -0,0 +1,323 @@
+const SeaportABIvVRF =  [
+  {
+    inputs: [
+      {
+        internalType: "uint64",
+        name: "subscriptionId",
+        type: "uint64",
+      },
+      {
+        internalType: "address",
+        name: "vrfCoordinator",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "link",
+        type: "address",
+      },
+      {
+        internalType: "bytes32",
+        name: "keyHash",
+        type: "bytes32",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "have",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "want",
+        type: "address",
+      },
+    ],
+    name: "OnlyCoordinatorCanFulfill",
+    type: "error",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "uint256[]",
+        name: "randomWords",
+        type: "uint256[]",
+      },
+    ],
+    name: "ReturnedRandomness",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "numerators",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "requestId",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256[]",
+        name: "randomWords",
+        type: "uint256[]",
+      },
+    ],
+    name: "rawFulfillRandomWords",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        components: [
+          {
+            components: [
+              {
+                internalType: "address",
+                name: "offerer",
+                type: "address",
+              },
+              {
+                internalType: "address",
+                name: "zone",
+                type: "address",
+              },
+              {
+                components: [
+                  {
+                    internalType: "enum ItemType",
+                    name: "itemType",
+                    type: "uint8",
+                  },
+                  {
+                    internalType: "address",
+                    name: "token",
+                    type: "address",
+                  },
+                  {
+                    internalType: "uint256",
+                    name: "identifierOrCriteria",
+                    type: "uint256",
+                  },
+                  {
+                    internalType: "uint256",
+                    name: "startAmount",
+                    type: "uint256",
+                  },
+                  {
+                    internalType: "uint256",
+                    name: "endAmount",
+                    type: "uint256",
+                  },
+                ],
+                internalType: "struct OfferItem[]",
+                name: "offer",
+                type: "tuple[]",
+              },
+              {
+                components: [
+                  {
+                    internalType: "enum ItemType",
+                    name: "itemType",
+                    type: "uint8",
+                  },
+                  {
+                    internalType: "address",
+                    name: "token",
+                    type: "address",
+                  },
+                  {
+                    internalType: "uint256",
+                    name: "identifierOrCriteria",
+                    type: "uint256",
+                  },
+                  {
+                    internalType: "uint256",
+                    name: "startAmount",
+                    type: "uint256",
+                  },
+                  {
+                    internalType: "uint256",
+                    name: "endAmount",
+                    type: "uint256",
+                  },
+                  {
+                    internalType: "address payable",
+                    name: "recipient",
+                    type: "address",
+                  },
+                ],
+                internalType: "struct ConsiderationItem[]",
+                name: "consideration",
+                type: "tuple[]",
+              },
+              {
+                internalType: "enum OrderType",
+                name: "orderType",
+                type: "uint8",
+              },
+              {
+                internalType: "uint256",
+                name: "startTime",
+                type: "uint256",
+              },
+              {
+                internalType: "uint256",
+                name: "endTime",
+                type: "uint256",
+              },
+              {
+                internalType: "bytes32",
+                name: "zoneHash",
+                type: "bytes32",
+              },
+              {
+                internalType: "uint256",
+                name: "salt",
+                type: "uint256",
+              },
+              {
+                internalType: "bytes32",
+                name: "conduitKey",
+                type: "bytes32",
+              },
+              {
+                internalType: "uint256",
+                name: "totalOriginalConsiderationItems",
+                type: "uint256",
+              },
+            ],
+            internalType: "struct OrderParameters",
+            name: "parameters",
+            type: "tuple",
+          },
+          {
+            internalType: "bytes",
+            name: "signature",
+            type: "bytes",
+          },
+        ],
+        internalType: "struct Order[]",
+        name: "orders",
+        type: "tuple[]",
+      },
+      {
+        components: [
+          {
+            components: [
+              {
+                internalType: "uint256",
+                name: "orderIndex",
+                type: "uint256",
+              },
+              {
+                internalType: "uint256",
+                name: "itemIndex",
+                type: "uint256",
+              },
+            ],
+            internalType: "struct FulfillmentComponent[]",
+            name: "offerComponents",
+            type: "tuple[]",
+          },
+          {
+            components: [
+              {
+                internalType: "uint256",
+                name: "orderIndex",
+                type: "uint256",
+              },
+              {
+                internalType: "uint256",
+                name: "itemIndex",
+                type: "uint256",
+              },
+            ],
+            internalType: "struct FulfillmentComponent[]",
+            name: "considerationComponents",
+            type: "tuple[]",
+          },
+        ],
+        internalType: "struct Fulfillment[]",
+        name: "fulfillments",
+        type: "tuple[]",
+      },
+    ],
+    name: "requestRandomWords",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "s_numWords",
+    outputs: [
+      {
+        internalType: "uint32",
+        name: "",
+        type: "uint32",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "s_randomWords",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "s_requestId",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
+export { SeaportABIvVRF };
---
+++ src/index.backup.ts
@@ -0,0 +1,140 @@
+import {
+    Contract,
+    ethers,
+} from "ethers";
+import {
+    Seaport
+} from "@opensea/seaport-js";
+import { SeaportABIvSME } from "./abi/Seaport_vSME";
+import type {
+    Seaport as SMESeaport,
+} from "./typechain-types/contracts/Seaport";
+import { MatchOrdersFulfillment } from "@opensea/seaport-js/lib/types";
+// Provider must be provided to the signer when supplying a custom signer
+const provider = new ethers.providers.JsonRpcProvider(
+  "https://eth-sepolia.public.blastapi.io"
+);
+const smeSeaportAddress = "0x45a7f5Ff630D31Eeb1e00dc24DF2f23DF1bA0A7C"
+const testERC20Address = "0x8D4E2c8bc6b1E4Fa0ED829E6786E9096dd6DC265"
+const testERC721Address = "0xE4E39D40d1b9c70dcd115FEA8DaEF242194f2cC7"
+
+
+const main = async () => {
+    const makerOrder = await build_maker_order();
+    const takerOrder = await build_taker_order();
+    const modeOrderFulfillments: MatchOrdersFulfillment[] = [];
+    modeOrderFulfillments.push({
+        offerComponents: [
+          {
+            orderIndex: 0,
+            itemIndex: 0,
+          },
+        ],
+        considerationComponents: [
+          {
+            orderIndex: 1,
+            itemIndex: 0,
+          },
+        ],
+      });
+
+      modeOrderFulfillments.push({
+        offerComponents: [
+          {
+            orderIndex: 1,
+            itemIndex: 0,
+          },
+        ],
+        considerationComponents: [
+          {
+            orderIndex: 0,
+            itemIndex: 0,
+          },
+        ],
+      });
+    const numerator = 1;
+    const denominator = 2;
+
+    console.log(makerOrder);
+    console.log(takerOrder);
+    console.log(modeOrderFulfillments);
+    const privateKey = "";
+    const Signer = new ethers.Wallet(privateKey, provider);
+    const smeContract = new Contract(
+        smeSeaportAddress,
+        SeaportABIvSME,
+        Signer,
+      ) as SMESeaport;
+    smeContract.matchOrdersWithLucky([makerOrder,takerOrder],modeOrderFulfillments,numerator,denominator)
+     .then(console.log)
+}
+
+async function build_taker_order() {
+    const privateKey = "";
+    const Signer = new ethers.Wallet(privateKey, provider);
+
+    const seaport = new Seaport(Signer, {overrides: {contractAddress: smeSeaportAddress}});
+
+    const offerer = "0x53B3F192A56a9cdA260476974443634a96529c72";
+    const { executeAllActions } = await seaport.createOrder(
+        {
+            zone: "0x0000000000000000000000000000000000000000",
+            startTime: Math.floor(new Date().getTime() / 1000).toString(),
+            endTime: Math.floor(new Date().getTime() / 1000 + 60 * 60).toString(),
+            consideration: [
+                {
+                    itemType: 2,
+                    token: testERC721Address,
+                    identifier: "1",
+                    recipient: offerer,
+                },
+            ],
+            offer: [
+                {
+                    amount: ethers.utils.parseEther("0.0002").toString(),
+                    token: testERC20Address,
+                    endAmount: ethers.utils.parseEther("0.0004").toString(),
+                },
+            ]
+        },
+        offerer
+    );
+
+    const order = await executeAllActions();
+    return order;
+}
+
+async function build_maker_order() {
+    const privateKey = "";
+    const Signer = new ethers.Wallet(privateKey, provider);
+
+    const seaport = new Seaport(Signer, {overrides: {contractAddress: smeSeaportAddress}});
+    const offerer = "0x28c73A60ccF8c66c14EbA8935984e616Df2926e3";
+    const { executeAllActions } = await seaport.createOrder(
+        {
+            zone: "0x0000000000000000000000000000000000000000",
+            startTime: Math.floor(new Date().getTime() / 1000).toString(),
+            endTime: Math.floor(new Date().getTime() / 1000 + 60 * 60).toString(),
+            offer: [
+                {
+                    itemType: 2,
+                    token: testERC721Address,
+                    identifier: "1",
+                },
+            ],
+            consideration: [
+                {
+                    amount: ethers.utils.parseEther("0.0002").toString(),
+                    token: testERC20Address,
+                    endAmount: ethers.utils.parseEther("0.0004").toString(),
+                    recipient: offerer,
+                },
+            ]
+        },
+        offerer
+    );
+
+    const order = await executeAllActions();
+    return order;
+}
+main();
